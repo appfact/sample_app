4498f3723dcc7114cc058dc6ed8059b39405fff1
Alex L <getshiftshark@gmail.com>
2013-05-18 11:00:48 +0100

    Finish contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,7 +6,7 @@ describe "Static pages" do
 
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_selector('Sample App')
+      page.should have_selector('h1', :text => 'Sample App')
     end
 
     it "should have the right title" do
@@ -21,7 +21,7 @@ describe "Static pages" do
 
     it "should have the content 'Help'" do
       visit '/static_pages/help'
-      page.should have_selector('Help')
+      page.should have_selector('h1', :text => 'Help')
     end
 
       it "should have the title 'Help'" do
@@ -35,12 +35,25 @@ describe "About page" do
 
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
-      page.should have_selector('About Us')
+      page.should have_selector('h1', :text => 'About Us')
     end
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
       page.should have_selector('title',
                     :text => "Ruby on Rails Tutorial Sample App | About Us")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact Page'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+    it "should have the title 'Contact Page'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+                    :text => "Ruby on Rails Tutorial Sample App | Contact Page")
     end
   end
 
